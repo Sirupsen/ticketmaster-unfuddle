69a39895a1d6ea421e21211656c91c1c6725a261
Simon <sirup@sirupsen.dk>
2010-06-24 00:33:47 +0200

    Added "main" file

---
+++ lib/provider/unfuddle.rb
@@ -0,0 +1,43 @@
+module TicketMaster::Provider
+  module Unfuddle
+    include TicketMaster::Provider::Base
+
+    def self.new(auth = {})
+      TicketMaster.new(:unfuddle, auth)
+    end
+
+    def authorize(authentication = {})
+      super
+      Unfuddler.authenticate(@authentication.to_hash)
+    end
+
+    def projects(*options)
+      authorize
+
+      projects = if options.length > 0
+                   Project.find(*options)
+                 else
+                   Project.find(:all)
+                 end
+
+      set_master_data(projects)
+    end
+
+    def project(*options)
+      authorize
+      return set_master_data(Project.find(:first, *options)) if options.length > 0
+      Project
+    end
+
+    private
+      def set_master_data(data)
+        if data.is_a?(Array)
+          data.collect! {|p| p.system_data.merge!(:master => self); p}
+        else
+          data.system_data.merge!(:master => self)
+        end
+
+        data
+      end
+  end
+end
